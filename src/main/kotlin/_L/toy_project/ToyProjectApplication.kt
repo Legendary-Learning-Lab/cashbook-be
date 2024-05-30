a1272fd0374e1efe2d23c9480aa5569fb5169564
MoonGD <king4mun@gmail.com>
2024-05-30 21:12:04 +0900

    init(main): spring 초기 환경 구축

---
+++ src/main/kotlin/_L/toy_project/ToyProjectApplication.kt
@@ -0,0 +1,11 @@
+package _L.toy_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToyProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToyProjectApplication>(*args)
+}
---
+++ src/test/kotlin/_L/toy_project/ToyProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package _L.toy_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToyProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
