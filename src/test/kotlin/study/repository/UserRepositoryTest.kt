b603c113ec7291b8c5e302816df1c9f7a6337511
MoonGD <king4mun@gmail.com>
2024-07-14 20:29:14 +0900

    feat: 기본 test 환경 구성

---
+++ src/test/kotlin/study/repository/UserRepositoryTest.kt
@@ -0,0 +1,12 @@
+package study.repository
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserRepositoryTest {
+    @Test
+    fun test() {
+        print("Hello World")
+    }
+}
