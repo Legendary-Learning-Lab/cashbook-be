40fb1e4655a3823386ed1b98e4f5603602f15104
MoonGD <king4mun@gmail.com>
2024-07-14 20:17:06 +0900

    fix: UserEntity class에 Entity 어노테이션 추가

--- src/main/kotlin/study/entity/UserEntity.kt
+++ src/main/kotlin/study/entity/UserEntity.kt
@@ -1,11 +1,13 @@
 package study.entity
 
 import jakarta.persistence.Column
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import study.enum.UserGender
 
+@Entity
 data class UserEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
